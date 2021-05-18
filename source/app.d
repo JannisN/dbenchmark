6d04ad2c51b6fcc23ae767bc08eacb80bbd2e9d1
JannisN <none@none.none>
2021-05-18 11:27:26 +0200

    m

--- source/app.d
+++ source/app.d
@@ -3,6 +3,8 @@ import core.time;
 import std.conv;
 import std.functional;
 import core.thread.osthread;
+import core.stdc.stdlib;
+import core.memory;
 
 struct Timer {
 	long ticks;
@@ -40,4 +42,26 @@ void main()
 	// hier könnte man eine lokale funktion benutzen um t in die funktion zu bringen(siehe delegates)
 	auto threadid = createLowLevelThread(cast(void delegate() nothrow)toDelegate(&threadtest));
 	joinLowLevelThread(threadid);
+	t.start();
+	auto ia = new int[100];
+	t.stop("Test");
+	t.start();
+	auto ia3 = new int[100];
+	ia3[12] = 123;
+	t.stop("Test");
+	t.start();
+	auto ia2 = malloc(100);
+	t.stop("Test");
+	t.start();
+	int[10] ia4;
+	ia4[0] = 12;
+	for (int i = 0; i < 100; i++) {}
+	t.stop("Test");
+	for (int i = 0; i < 1000000; i++) {new int[1000];}
+	t.start();
+	GC.collect();
+	t.stop("Collect");
 }
+
+//extern(C) __gshared bool rt_cmdline_enabled = false;
+//extern(C) __gshared string[] rt_options = ["gcopt=gc:manual disable:1"];
